aef2d545211c27ef6ce36ec29358d1ae53addebd
therealmaxkim <gk1473@nyu.edu>
2020-03-11 20:33:48 -0400

    Revert "updated graph tabs"
    This reverts commit 96eb28f6b41cbe4879f4aa95c41f8d7ca969a1ad.

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,31 +1,14 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Graph from "./Graph";
-import { Tabs, Tab, TabList, Panel, PanelList } from 'react-tabtab';
-import * as customStyle from 'react-tabtab/lib/themes/material-design';
+import Config from "./Config";
 
 class App extends Component {
     render() {
         return (
             <div>
-                <Tabs customStyle={customStyle}>
-                    <TabList>
-                        <Tab>Hourly</Tab>
-                        <Tab>Daily</Tab>
-                        <Tab>Weekly</Tab>
-                    </TabList>
-                    <PanelList>
-                        <Panel>
-                            <Graph type={'hourly'}/>
-                        </Panel>
-                        <Panel>
-                            <Graph type={'daily'}/>    
-                        </Panel>
-                        <Panel>
-                            <Graph type={'weekly'}/>
-                        </Panel>
-                    </PanelList>
-                </Tabs>
+                <div><Graph /></div>
+                <div><Config onSent={doFetchValues}/></div>
             </div>
         );
     }
@@ -33,4 +16,4 @@ class App extends Component {
 
 export default App;
 
-ReactDOM.render(<App />, document.getElementById("main"));
+ReactDOM.render(<App/>, document.getElementById("main"));
